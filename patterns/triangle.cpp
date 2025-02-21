4fdec6a5895040464f1f406d86222845e930f355
Pratiksha <kshirsagarpratiksha1820@gmail.com>
2025-02-21 23:07:20 +0530

    back

---
+++ patterns/triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 4;
+    char ch = 'A';
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            // cout << i+1 ;
+            cout << ch;
+        }
+        ch++;
+        cout << endl;
+    }
+
+    return 0;
+}
